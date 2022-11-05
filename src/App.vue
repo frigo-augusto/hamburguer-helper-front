d0309095ac6a76843dab06857f38c294cde08a4c
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2022-11-05 20:10:09 -0300

    Adicionada tela inicial

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main id="app">
+    <section id="center-container">
+      <h1>Hamburguer helper</h1>
+      <div id="buttons-container">
+        <button type="button" class="btn btn-primary">Atendente</button>
+        <button type="button" class="btn btn-primary">Cozinheiro</button>
+        <button type="button" class="btn btn-primary">Caixa</button>
+      </div>
+    </section>
+
+  </main>
 </template>
 
 <script>
@@ -32,26 +25,33 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color: gray;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  justify-content: center;
+  display: flex;
+  min-height: 700px;
 }
 
 h1, h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#center-container{
+  width: 30%;
+  background-color: yellow;
+  margin-top: 150px;
+  margin-bottom: 150px;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+#buttons-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button{
+  width: 150px;
+  padding: 30px;
 }
 
 a {
